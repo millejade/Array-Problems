6fe7d7e52c54146bf5812b3a32c31cf3aed5d8a3
Mille Jade Cabrera <36434363+millejade@users.noreply.github.com>
2021-11-19 21:19:02 +0800

    Update FindAllDuplicatesInArray.java

--- FindAllDuplicatesInArray.java
+++ FindAllDuplicatesInArray.java
@@ -1,3 +1,18 @@
+/*
+
+Find the duplicates in array
+
+1 <= array[index] <= n
+
+where n = length of array
+
+Runtime:
+Time Complexity = O(n) or linear time
+Space Complexity = O(1) or constant space
+                 = O(n) if you consider the result (which is a 'duplicates' list) as an extra space
+
+*/
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +28,23 @@ public class FindAllDuplicatesInArray {
 
     private static List finder(int[] arr) {
         
-        List<Integer> duplicates = new ArrayList<>();
+        List<Integer> duplicates = new ArrayList<>(); //List where you can store the duplicate numbers
         
+        //Iterate through an array
         for(int i = 0; i < arr.length; i++){
+            
+            //at i = 0, val = 4, then x = |4| - 1, then x = 3
             int x = Math.abs(arr[i]) - 1;
+            
+            //at arr[3] = 2 which is positive, then skip this IF STATEMENT
             if(arr[x] < 0){
                 duplicates.add(x + 1);
             }
+            
+            //flip the sign of the value, then arr[3] = -2
             arr[x] = (arr[x]) * -1;
+            
+            //it will just continue until we break out of a for loop
         }
         return duplicates;
     }
